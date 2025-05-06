2417264ba8db264056cbf1c619fdf779750927ba
Suk-Be Jang <s.jang@sokdesign.de>
2025-05-06 15:20:33 +0200

    fix typescript support for build process

---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+
+// https://vitejs.dev/config/
+export default ({ mode }: { mode: string }) => {
+	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+	return defineConfig({
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		server: {
+			proxy: {
+				'/api': process.env.VITE_BACKEND_URL || 'http://localhost:3000',
+			},
+		},
+	});
+};
