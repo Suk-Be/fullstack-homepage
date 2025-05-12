7d32903f0f84798702b6bed3fe552136bbd61a83
Suk-Be Jang <s.jang@sokdesign.de>
2025-05-12 16:13:14 +0200

    refactor HP tests

--- frontend/src/tests/pages/HomePage.test.tsx
+++ frontend/src/tests/pages/HomePage.test.tsx
@@ -1,48 +1,66 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
+import { HPProps } from '../../data/HomePage';
 import { navigateTo } from '../utils';
 
 describe('HomePage', () => {
-    it('should render the Logo', async () => {
+    const renderUtil = () => {
         navigateTo('/');
 
         const heading = screen.getAllByRole('heading', {
             name: /Suk-Be Jang/i,
         });
+        const logo = heading[1];
         const claim = screen.getAllByText(/web developer/i);
-
-        expect(claim[1]).toBeInTheDocument();
-        expect(heading[1]).toBeInTheDocument();
-    });
-    it('should render Home Page Link', async () => {
-        navigateTo('/');
+        const logoClaim = claim[1];
 
         const linkHomePage = screen.getAllByRole('link');
+        const linkHP = linkHomePage[0];
+
+        const buttons = screen.getAllByRole('button');
+        const mainMenu = buttons[0];
 
         const user = userEvent.setup();
-        await user.click(linkHomePage[0]);
 
-        const heading = screen.getAllByRole('heading', {
-            name: /Suk-Be Jang/i,
-        });
-        const claim = screen.getAllByText(/web developer/i);
+        const headline = screen.getByText(HPProps.data[0].attributes.title);
 
-        expect(claim[1]).toBeInTheDocument();
-        expect(heading[1]).toBeInTheDocument();
+        return {
+            logo,
+            logoClaim,
+            user,
+            linkHP,
+            headline,
+            buttons,
+            mainMenu,
+        };
+    };
+    it('should render the Logo', async () => {
+        const { logo, logoClaim } = renderUtil();
+
+        expect(logo).toBeInTheDocument();
+        expect(logoClaim).toBeInTheDocument();
     });
-    it('should render Main Menu Links', async () => {
-        navigateTo('/');
+    it('should render Home Page Link', async () => {
+        const { linkHP } = renderUtil();
 
-        const menuButton = screen.getAllByRole('button');
+        expect(linkHP).toBeInTheDocument();
+    });
+    it('should render Main Menu and a link from it', async () => {
+        const { user, mainMenu } = renderUtil();
 
-        const user = userEvent.setup();
-        await user.click(menuButton[0]);
-        // screen.debug(menuItemHomePage[0]);
+        await user.click(mainMenu);
+
         const menuItemPlaygroundPage = screen.getByRole('link', {
             name: /PlaygroundPage/i,
         });
 
         expect(menuItemPlaygroundPage).toBeInTheDocument();
+    });
+
+    it('should render headline', async () => {
+        const { headline } = renderUtil();
+
+        expect(headline).toBeInTheDocument();
     });
 });
