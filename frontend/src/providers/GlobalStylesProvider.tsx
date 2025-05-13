7528bc0308b90f08dfdc7d2696d67122d1e055c4
Suk-Be Jang <s.jang@sokdesign.de>
2025-05-13 15:41:28 +0200

    get rid of roboto font and only css reset from mui global styles

--- frontend/src/providers/GlobalStylesProvider.tsx
+++ frontend/src/providers/GlobalStylesProvider.tsx
@@ -1,11 +1,7 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const GlobalStylesProvider = () => {
-	return <CssBaseline />;
+    return <CssBaseline />;
 };
 
 export default GlobalStylesProvider;
