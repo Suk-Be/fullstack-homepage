f6eaf506463dc00cd1dad80269aae396589f0d41
Suk-Be Jang <s.jang@sokdesign.de>
2025-05-30 09:45:17 +0200

    test for singing in

---
+++ frontend/src/tests/components/auth/SignUp.test.tsx
@@ -0,0 +1,211 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it } from 'vitest';
+import ErrorMessages from '../../../data/ErrorMessages';
+import userFactory from '../../mocks/factories/userFactories';
+import { navigateTo } from '../../utils';
+
+describe('SignUp', () => {
+    const renderStatic = () => {
+        navigateTo('/'); // Render HomePage
+        const title = screen.getByRole('heading', { name: /Registrierung/i });
+        const registerIcon = screen.getByTestId('HowToRegIcon');
+        const descriptionRegistration = screen.getByTestId('description-sign-up');
+        const infoSwitchToLogin = screen.getByTestId('info-switch-to-login');
+        const linkSwitchToLogin = screen.getByRole('link', { name: /log in/i });
+
+        return {
+            title,
+            registerIcon,
+            descriptionRegistration,
+            infoSwitchToLogin,
+            linkSwitchToLogin,
+        };
+    };
+
+    const renderRegistrationForm = () => {
+        const user = userEvent.setup();
+        const fakeUser = userFactory();
+
+        navigateTo('/'); // Render HomePage
+
+        const userInput = screen.getByLabelText(/user name/i);
+        const emailInput = screen.getByLabelText(/email/i);
+        const passwordInput = screen.getByLabelText('Passwort');
+        const passwordConfirmationInput = screen.getByLabelText(/passwort bestätigung/i);
+        const termsConfirmationCheckbox = screen.getByRole('checkbox', {
+            name: /Ich habe die Bedingungen gelesen nund stimme zu/i,
+        });
+
+        const registerButton = screen.getByTestId('form-button-register');
+
+        return {
+            userInput,
+            emailInput,
+            passwordInput,
+            passwordConfirmationInput,
+            termsConfirmationCheckbox,
+            registerButton,
+            user,
+            fakeUser,
+        };
+    };
+
+    it('should render a title, register icon and and a register description', () => {
+        const {
+            title,
+            registerIcon,
+            descriptionRegistration,
+            infoSwitchToLogin,
+            linkSwitchToLogin,
+        } = renderStatic();
+
+        expect(title).toHaveTextContent(/registrierung/i);
+        expect(registerIcon).toBeInTheDocument();
+        expect(descriptionRegistration).toHaveTextContent(
+            /Bitte registrieren Sie sich, um sich hier hinterlegte Prototypen Projekte anschauen zu können./i,
+        );
+        expect(infoSwitchToLogin).toHaveTextContent(/Verfügen Sie über ein Konto/i);
+        expect(linkSwitchToLogin).toBeInTheDocument();
+    });
+
+    it('should render a default registration form on load', () => {
+        const {
+            userInput,
+            emailInput,
+            passwordInput,
+            passwordConfirmationInput,
+            termsConfirmationCheckbox,
+        } = renderRegistrationForm();
+
+        expect(userInput).toHaveAttribute('placeholder', 'Jon Snow');
+        expect(emailInput).toHaveAttribute('placeholder', 'your@email.com');
+        expect(passwordInput).toHaveAttribute('placeholder', '••••••');
+        expect(passwordConfirmationInput).toHaveAttribute('placeholder', '••••••');
+        expect(termsConfirmationCheckbox).not.toBeChecked();
+    });
+
+    it('should render error messages on submit, if required inputs are missing', async () => {
+        const {
+            registerButton,
+            user,
+            userInput,
+            emailInput,
+            passwordInput,
+            passwordConfirmationInput,
+            termsConfirmationCheckbox,
+            fakeUser,
+        } = renderRegistrationForm();
+
+        await user.click(registerButton);
+
+        const errorUserInput = screen.getByText(ErrorMessages.SignUp.name);
+        const errorEmailInput = screen.getByText(ErrorMessages.SignUp.email);
+        const errorPasswordInput = screen.getByText(ErrorMessages.SignUp.password);
+        const errorPasswordConfirmationInput = screen.getByText(
+            ErrorMessages.SignUp.password_confirmation,
+        );
+        const errorTermsConfirmationCheckbox = screen.getByText(ErrorMessages.SignUp.terms);
+
+        expect(errorUserInput).toBeInTheDocument();
+        expect(errorEmailInput).toBeInTheDocument();
+        expect(errorPasswordInput).toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type user name
+        await user.type(userInput, fakeUser.name);
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+
+        expect(errorEmailInput).toBeInTheDocument();
+        expect(errorPasswordInput).toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type wrong email address
+        await user.type(emailInput, fakeUser.name);
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+
+        expect(errorEmailInput).toBeInTheDocument();
+        expect(errorPasswordInput).toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type right email address
+        await user.clear(emailInput);
+        await user.type(emailInput, fakeUser.email);
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+
+        expect(errorPasswordInput).toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type wrong password length
+        await user.type(passwordInput, '1234567');
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+
+        expect((passwordInput as HTMLInputElement).value.length).toBeLessThan(8);
+        expect(errorPasswordInput).toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type right password length
+        await user.clear(passwordInput);
+        await user.type(passwordInput, fakeUser.password);
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+        expect((passwordInput as HTMLInputElement).value.length).not.toBeLessThan(8);
+        expect(errorPasswordInput).not.toBeInTheDocument();
+
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type password confirmation, but with wrong match
+        await user.type(passwordConfirmationInput, 'notSame');
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+        expect((passwordInput as HTMLInputElement).value.length).not.toBeLessThan(8);
+        expect(errorPasswordInput).not.toBeInTheDocument();
+
+        expect(errorPasswordConfirmationInput).toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        // type right password confirmation, password is set 12345678
+        await user.clear(passwordConfirmationInput);
+        await user.type(passwordConfirmationInput, fakeUser.password);
+        await user.click(registerButton);
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+        expect((passwordInput as HTMLInputElement).value.length).not.toBeLessThan(8);
+        expect(errorPasswordInput).not.toBeInTheDocument();
+
+        expect(errorPasswordConfirmationInput).not.toBeInTheDocument();
+        expect(errorTermsConfirmationCheckbox).toBeInTheDocument();
+
+        //confirm terms
+        await user.click(termsConfirmationCheckbox);
+        await user.click(registerButton);
+
+        expect(errorTermsConfirmationCheckbox).not.toBeInTheDocument();
+
+        expect(errorUserInput).not.toBeInTheDocument();
+        expect(errorEmailInput).not.toBeInTheDocument();
+        expect(errorPasswordInput).not.toBeInTheDocument();
+        expect(errorPasswordConfirmationInput).not.toBeInTheDocument();
+    });
+});
