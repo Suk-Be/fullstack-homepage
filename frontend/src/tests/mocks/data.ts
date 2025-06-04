aab925a205358a278c27e007537de88f07122bd7
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-04 13:32:01 +0200

    add a mock registered user

--- frontend/src/tests/mocks/data.ts
+++ frontend/src/tests/mocks/data.ts
@@ -3,3 +3,10 @@ export const categoryData = [
     { id: 2, name: 'Beauty' },
     { id: 3, name: 'Gardening' },
 ];
+
+export const registeredUserData = {
+    id: '1',
+    name: 'John Doe',
+    email: 'existing@example.com',
+    password: 'password123',
+};
