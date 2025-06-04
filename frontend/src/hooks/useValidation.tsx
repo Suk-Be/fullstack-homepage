b154d22e1471000ef724f80032391037d03d9dc2
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-04 13:15:42 +0200

    delete obsolete console.log

--- frontend/src/hooks/useValidation.tsx
+++ frontend/src/hooks/useValidation.tsx
@@ -65,14 +65,6 @@ export default function useValidateInputs(checked: boolean) {
             setCheckedErrorMessage('');
         }
 
-        // console.log('form inputs', {
-        //     name: name.value,
-        //     email: email.value,
-        //     password: password.value,
-        //     passwordConfirmation: passwordConfirmation.value,
-        //     isValid: isValid,
-        // });
-
         return isValid;
     };
 
