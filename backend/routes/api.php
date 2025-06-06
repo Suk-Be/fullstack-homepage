27a3bb4035e9dcd6102b77ad0a339a6312db3ecd
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-06 07:50:54 +0200

    delete not needed and unsecure route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,10 +2,8 @@
 
 use App\Http\Controllers\Api\Auth\Spa\AuthController;
 use App\Http\Controllers\Api\Auth\Spa\SocialiteController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', fn(Request $request) => $request->user());
 Route::middleware('auth:sanctum')->get('/me', [AuthController::class, 'me']);
 
 Route::prefix('auth/spa')->group(function () {
