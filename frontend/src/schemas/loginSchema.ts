6757b5c8992a008a72ca63ff8a69844c5402b8e2
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-17 14:56:01 +0200

    type formatted errors

--- frontend/src/schemas/loginSchema.ts
+++ frontend/src/schemas/loginSchema.ts
@@ -1,6 +1,10 @@
 import z from 'zod';
+import ErrorMessages from '../data/ErrorMessages';
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, 'Password is required'),
+    email: z.string().email({ message: ErrorMessages.SignIn.email }),
+    password: z.string().min(8, { message: ErrorMessages.SignIn.password }),
+    responseEmail: z.string().email({ message: ErrorMessages.SignIn.responseEmail }),
 });
+
+export type LoginFormattedErrors = z.inferFormattedError<typeof LoginSchema>;
