4b518475a34327d4e2fa3cb51d1ad4db397a1bd4
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-20 17:04:02 +0200

    fix import

--- frontend/src/pages/PlaygroundPage.tsx
+++ frontend/src/pages/PlaygroundPage.tsx
@@ -1,12 +1,12 @@
 import { Box } from '@mui/material';
 import LoginTest from '../components/LoginTest';
-import SignUp from '../components/auth/SignUp';
+import ToggleSignIn from '../components/auth/index';
 
 const PlaygroundPage = () => {
     return (
         <Box sx={{ padding: '7rem 0rem' }}>
             <LoginTest />
-            <SignUp />
+            <ToggleSignIn />
         </Box>
     );
 };
