f89b608eaa0aa3fd4d7a11f204dceeddf59750e6
Suk-Be Jang <s.jang@sokdesign.de>
2025-06-26 17:27:39 +0200

    implement german locale

---
+++ backend/lang/de/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'reset' => 'Dein Passwort wurde zurückgesetzt.',
+    'sent' => 'Wir haben dir den Link zum Zurücksetzen deines Passworts per E-Mail geschickt.',
+    'throttled' => 'Bitte warte, bevor du es erneut versuchst.',
+    'token' => 'Dieses Passwort-Zurücksetzungstoken ist ungültig.',
+    'user' => 'Es konnte kein Benutzer mit dieser E-Mail-Adresse gefunden werden.', // ← Customize this
+];
