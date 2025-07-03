5911293fa02c967b2aa2f975ac27f5aec9d6b167
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-03 10:57:28 +0200

    automatic login in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import { useEffect } from 'react';
 import './App.css';
+import requestMe from './components/auth/SignUp/requestMe';
 import Layout from './pages/Layout';
-import initializeCookie from './plugins/initializeCookies';
+import initializeCookies from './plugins/initializeCookies';
 import Providers from './providers';
 
 function App() {
     useEffect(() => {
-        initializeCookie();
+        const initAuth = async () => {
+            await initializeCookies();
+
+            try {
+                await requestMe(true); // or LaravelApiClient.get('/api/user')
+            } catch (error) {
+                console.log('[Auth] No active session found');
+            }
+        };
+
+        initAuth();
     }, []);
 
     return (
