2cfe63ad2d94e7342ac5e697d5cbc5bba3c83cbd
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-08 10:38:18 +0200

    refactor to one parameter

--- frontend/src/tests/components/footer/index.test.tsx
+++ frontend/src/tests/components/footer/index.test.tsx
@@ -1,13 +1,13 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
-import { navigateTo, PathAndReduxState } from '../../utils/testRenderUtils';
+import { navigateTo } from '../../utils/testRenderUtils';
 
 describe('BasicMenu', () => {
-    const renderUtils = ({ route, preloadedState }: PathAndReduxState) => {
+    const renderUtils = ({route}: {route: string}) => {
         const user = userEvent.setup();
 
-        navigateTo({ route, preloadedState }); // Render HomePage
+        navigateTo({route}); // Render HomePage
 
         const impressumLink = screen.getByTestId('link-impressum-page');
         const datenschutzLink = screen.getByTestId('link-datenschutz-page');
@@ -39,7 +39,7 @@ describe('BasicMenu', () => {
     ])(
         'should render a footer on $page with a link to impressum and datenschutz page',
         ({ route }) => {
-            const { impressumLink, datenschutzLink } = renderUtils({ route, preloadedState: {} });
+            const { impressumLink, datenschutzLink } = renderUtils({route});
 
             expect(impressumLink).toBeInTheDocument();
             expect(datenschutzLink).toBeInTheDocument();
