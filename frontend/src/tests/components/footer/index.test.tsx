17dfa305ee00b912b7393146026c15b890cdc80d
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-15 10:52:12 +0200

    fix footer test: main menu links needed global login

--- frontend/src/tests/components/footer/index.test.tsx
+++ frontend/src/tests/components/footer/index.test.tsx
@@ -1,13 +1,13 @@
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
-import { navigateTo } from '../../utils/testRenderUtils';
+import { navigateTo, PathAndReduxState } from '../../utils/testRenderUtils';
 
 describe('BasicMenu', () => {
-    const renderUtils = ({route}: {route: string}) => {
+    const renderUtils = ({ route, preloadedState }: PathAndReduxState) => {
         const user = userEvent.setup();
 
-        navigateTo({route}); // Render HomePage
+        navigateTo({ route, preloadedState }); // Render HomePage
 
         const impressumLink = screen.getByTestId('link-impressum-page');
         const datenschutzLink = screen.getByTestId('link-datenschutz-page');
@@ -32,14 +32,32 @@ describe('BasicMenu', () => {
             page: 'DatenschutzPage',
             route: '/datenschutz',
         },
+    ])(
+        'should render a footer on $page with a link to impressum and datenschutz page if not logged in',
+        ({ route }) => {
+            const { impressumLink, datenschutzLink } = renderUtils({ route });
+
+            expect(impressumLink).toBeInTheDocument();
+            expect(datenschutzLink).toBeInTheDocument();
+        },
+    );
+
+    it.each([
+        {
+            page: 'ProjectTemplateEnginePage',
+            route: '/template-engine',
+        },
         {
             page: 'PlaygroundPage',
             route: '/playground',
         },
     ])(
-        'should render a footer on $page with a link to impressum and datenschutz page',
+        'should render a footer on $page with a link to impressum and datenschutz page if logged in',
         ({ route }) => {
-            const { impressumLink, datenschutzLink } = renderUtils({route});
+            const { impressumLink, datenschutzLink } = renderUtils({
+                route,
+                preloadedState: { login: { isLoggedIn: true } },
+            });
 
             expect(impressumLink).toBeInTheDocument();
             expect(datenschutzLink).toBeInTheDocument();
