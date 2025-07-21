d001b284ab8a98be5379fedeb5287a3b05f9368d
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-21 14:49:31 +0200

    adjut test to assert additional isLoading state

--- frontend/src/tests/store/loginSlice.test.ts
+++ frontend/src/tests/store/loginSlice.test.ts
@@ -6,18 +6,21 @@ describe('loginSlice', () => {
         // since redux does not know what to do with it. it returns the default state
         expect(loginReducer(undefined, { type: '@@INIT' })).toEqual({
             isLoggedIn: false,
+            isLoading: true,
         });
     });
 
     it('should login when login is dispatched', () => {
-        expect(loginReducer({ isLoggedIn: false }, login())).toEqual({
+        expect(loginReducer({ isLoggedIn: false, isLoading: false }, login())).toEqual({
             isLoggedIn: true,
+            isLoading: false
         });
     });
 
     it('should logout when logout is dispatched', () => {
-        expect(loginReducer({ isLoggedIn: true }, logout())).toEqual({
+        expect(loginReducer({ isLoggedIn: true, isLoading: false }, logout())).toEqual({
             isLoggedIn: false,
+            isLoading: false
         });
     });
 });
