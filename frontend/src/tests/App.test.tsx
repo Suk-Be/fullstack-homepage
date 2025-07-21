f55a44fe5ed9bbba8edba978b9707cbf85001b68
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-21 18:59:59 +0200

    update test

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -1,20 +1,11 @@
 import App from '@/App';
-import * as AuthHook from '@/hooks/auth/useAuthInit'; // Import the module containing useAuthInit
 import { screen } from '@testing-library/react';
-import { vi } from 'vitest';
 import { renderWithProviders } from './utils/testRenderUtils'; // Adjust path to your test utility
 
-vi.spyOn(AuthHook, 'useAuthInit').mockImplementation(() => {});
-
-vi.mock('../pages/Layout', () => ({
-    default: () => <div data-testid="layout-component">Mocked Layout</div>,
-}));
-
 describe('App', () => {
-    it('should call useAuthInit and render the Layout component', () => {
+    it('should render the Layout component', () => {
         renderWithProviders(<App />);
 
-        expect(AuthHook.useAuthInit).toHaveBeenCalledTimes(1);
-        expect(screen.getByTestId('layout-component')).toBeInTheDocument();
+        expect(screen.getByTestId('main')).toBeInTheDocument();
     });
 });
