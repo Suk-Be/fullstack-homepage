8ee90fbb891e29cd7e87fcc467a6fd74b9c90538
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-22 16:08:52 +0200

    add tests for route

--- frontend/src/tests/routes.test.tsx
+++ frontend/src/tests/routes.test.tsx
@@ -1,9 +1,23 @@
 
-import { screen } from '@testing-library/react';
+import ResetPasswordPage from '@/pages/ResetPasswordPage';
+import { store } from '@/store';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
+import { registeredUserData } from './mocks/data';
+import { db } from './mocks/db';
 import { navigateTo } from './utils/testRenderUtils';
 
 describe('routes', () => {
+  beforeEach(() => {
+      db.user.create({
+          id: registeredUserData.id,
+          name: registeredUserData.name,
+          email: registeredUserData.email,
+      });
+  });
+
   it.each([
     {
       path: '/',
@@ -17,14 +31,7 @@ describe('routes', () => {
       component: 'ImprintPage',
       level: 1
     },
-    // Text Website: Ungültiger oder fehlender Passwort-Reset-Token. 
-    // if caling route with no token provided by backend
-    {
-      path: '/reset-password',
-      elementHeadline: /Ungültiger oder fehlender Passwort-Reset-Token./i,
-      component: 'ResetPasswordPage',
-      level: 5
-    },
+    
     {
       path: '/template-engine',
       elementHeadline: /layout presets/i,
@@ -58,4 +65,18 @@ describe('routes', () => {
     const heading = await screen.findByRole('heading', { name: elementHeadline, level: level }); 
     expect(heading).toBeInTheDocument();
   });
+
+  it('should render the ResetPassworPage', async () => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/reset-password?token=test123&email=test@example.com']}>
+          <ResetPasswordPage />
+        </MemoryRouter>
+      </Provider>
+    );
+    
+    const heading = await screen.findByRole('heading', { name: /Passwort zurücksetzen/i });
+    expect(heading).toBeInTheDocument();
+  });
+  
 });
