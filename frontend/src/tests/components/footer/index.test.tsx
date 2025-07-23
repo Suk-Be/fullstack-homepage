a3e91e60a2f9077ad190c6389858042cf5ec22ae
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-23 13:37:08 +0200

    fix test due to changes in router.tsx just rendering component with different urls

--- frontend/src/tests/components/footer/index.test.tsx
+++ frontend/src/tests/components/footer/index.test.tsx
@@ -1,13 +1,14 @@
-import { navigateTo, PathAndReduxState } from '@/tests/utils/testRenderUtils';
+import Footer from '@/components/footer';
+import { PathAndReduxState, renderWithProviders } from '@/tests/utils/testRenderUtils';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 
-describe('BasicMenu', () => {
+describe('Footer', () => {
     const renderUtils = ({ route, preloadedState }: PathAndReduxState) => {
         const user = userEvent.setup();
 
-        navigateTo({ route, preloadedState }); // Render HomePage
+        renderWithProviders(<Footer />, { route, preloadedState }); // Render HomePage
 
         const impressumLink = screen.getByTestId('link-impressum-page');
         const datenschutzLink = screen.getByTestId('link-datenschutz-page');
@@ -32,32 +33,22 @@ describe('BasicMenu', () => {
             page: 'DatenschutzPage',
             route: '/datenschutz',
         },
+        {
+            route: '/template-engine',
+            page: 'ProjectTemplateEnginePage',
+        },
+        {
+            route: '/template-engine/presets',
+            page: 'ProjectTemplateEnginePresetsPage',
+        },
+        {
+            route: '/playground',
+            page: 'PlaygroundPage',
+        },
     ])(
         'should render a footer on $page with a link to impressum and datenschutz page if not logged in',
         ({ route }) => {
             const { impressumLink, datenschutzLink } = renderUtils({ route });
-
-            expect(impressumLink).toBeInTheDocument();
-            expect(datenschutzLink).toBeInTheDocument();
-        },
-    );
-
-    it.each([
-        {
-            page: 'ProjectTemplateEnginePage',
-            route: '/template-engine',
-        },
-        {
-            page: 'PlaygroundPage',
-            route: '/playground',
-        },
-    ])(
-        'should render a footer on $page with a link to impressum and datenschutz page if logged in',
-        ({ route }) => {
-            const { impressumLink, datenschutzLink } = renderUtils({
-                route,
-                preloadedState: { login: { isLoggedIn: true } },
-            });
 
             expect(impressumLink).toBeInTheDocument();
             expect(datenschutzLink).toBeInTheDocument();
