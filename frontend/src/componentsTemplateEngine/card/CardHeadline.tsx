d2ff5fdc87b57aae89e99734bac799955d8a2a64
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-23 17:28:09 +0200

    delete inertia homepage teasers from old project

--- frontend/src/componentsTemplateEngine/card/CardHeadline.tsx
+++
@@ -1,7 +0,0 @@
-import { FC, ComponentPropsWithoutRef } from 'react';
-
-const CardHeadline: FC<ComponentPropsWithoutRef<'h2'>> = ({ children }) => {
-    return <h2 className="text-xl font-semibold">{children}</h2>;
-};
-
-export default CardHeadline;
