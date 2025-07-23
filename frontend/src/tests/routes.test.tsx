19bfa68a4da6ae69a76623ad9c2d116cee7b46ee
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-23 13:39:13 +0200

    fix  tests

--- frontend/src/tests/routes.test.tsx
+++ frontend/src/tests/routes.test.tsx
@@ -1,7 +1,17 @@
+import HomePage from '@/pages/HomePage';
+import ImprintPage from '@/pages/ImprintPage';
+import PlaygroundPage from '@/pages/PlaygroundPage';
+import ProjectTemplateEnginePage from '@/pages/ProjectTemplateEnginePage';
+import ProjectTemplateEnginePresetsPage from '@/pages/ProjectTemplateEnginePresetsPage';
 import ResetPasswordPage from '@/pages/ResetPasswordPage';
 import { store } from '@/store';
-import { navigateTo } from '@/tests/utils/testRenderUtils';
+import {
+    navigateTo,
+    renderWithProviders,
+    renderWithProvidersDOM,
+} from '@/tests/utils/testRenderUtils';
 import { render, screen } from '@testing-library/react';
+import { ReactElement } from 'react';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
@@ -9,73 +19,140 @@ import { registeredUserData } from './mocks/data';
 import { db } from './mocks/db';
 
 describe('routes', () => {
-  beforeEach(() => {
-      db.user.create({
-          id: registeredUserData.id,
-          name: registeredUserData.name,
-          email: registeredUserData.email,
-      });
-  });
-
-  it.each([
-    {
-      path: '/',
-      elementHeadline: /suk-be jang/i,
-      component: 'HomePage',
-      level: 1
-    },
-    {
-      path: '/impressum',
-      elementHeadline: /impressum/i,
-      component: 'ImprintPage',
-      level: 1
-    },
-    
-    {
-      path: '/template-engine',
-      elementHeadline: /layout presets/i,
-      component: 'ProjectTemplateEnginePage',
-      level: 2
-    },
-    {
-      path: '/playground',
-      elementHeadline: /playground/i,
-      component: 'PlaygroundPage',
-      level: 1
-    },
-    {
-      path: '/does-not-exist',
-      elementHeadline: /seite nicht gefunden/i,
-      component: 'ErrorPage',
-      level: 5
-    }
-
-  ])
-  ('should render $component with $path', async ({path, elementHeadline, level}: {path: string, elementHeadline: RegExp | string, level: number}) => {
-    navigateTo({
-      route: path, 
-      preloadedState: { 
-        login: {
-          isLoggedIn: true, 
-          isLoading: false
-        }
-      }
+    beforeEach(() => {
+        db.user.create({
+            id: registeredUserData.id,
+            name: registeredUserData.name,
+            email: registeredUserData.email,
+        });
     });
-    const heading = await screen.findByRole('heading', { name: elementHeadline, level: level }); 
-    expect(heading).toBeInTheDocument();
-  });
 
-  it('should render the ResetPasswordPage', async () => {
-    render(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['/reset-password?token=test123&email=test@example.com']}>
-          <ResetPasswordPage />
-        </MemoryRouter>
-      </Provider>
+    it.each([
+        {
+            path: '/',
+            elementHeadline: /suk-be jang/i,
+            page: 'HomePage',
+            component: <HomePage />,
+            level: 1,
+        },
+        {
+            path: '/impressum',
+            elementHeadline: /impressum/i,
+            page: 'ImprintPage',
+            component: <ImprintPage />,
+            level: 1,
+        },
+        {
+            path: '/playground',
+            elementHeadline: /playground/i,
+            page: 'PlaygroundPage',
+            component: <PlaygroundPage />,
+            level: 1,
+        },
+    ])(
+        'should render $page with $path',
+        async ({
+            component,
+            path,
+            elementHeadline,
+            level,
+        }: {
+            component: ReactElement;
+            path: string;
+            elementHeadline: RegExp | string;
+            level: number;
+        }) => {
+            renderWithProviders(component, {
+                route: path,
+                preloadedState: {
+                    login: {
+                        isLoggedIn: true,
+                        isLoading: false,
+                    },
+                },
+            });
+            const heading = await screen.findByRole('heading', {
+                name: elementHeadline,
+                level: level,
+            });
+            expect(heading).toBeInTheDocument();
+        },
     );
-    
-    const heading = await screen.findByRole('heading', { name: /Passwort zurücksetzen/i });
-    expect(heading).toBeInTheDocument();
-  });
-  
+
+    it.each([
+        {
+            path: '/template-engine',
+            elementHeadline: 'Layout Presets',
+            page: 'ProjectTemplateEnginePage',
+            component: <ProjectTemplateEnginePage />,
+            level: 2,
+        },
+        {
+            path: '/template-engine/presets',
+            elementHeadline: 'Grid Layout Presets',
+            page: 'ProjectTemplateEnginePresetsPage',
+            component: <ProjectTemplateEnginePresetsPage />,
+            level: 1,
+        },
+    ])(
+        'should render $page with $path',
+        async ({
+            component,
+            path,
+            elementHeadline,
+            level,
+        }: {
+            component: ReactElement;
+            path: string;
+            elementHeadline: RegExp | string;
+            level: number;
+        }) => {
+            renderWithProvidersDOM(component, {
+                route: path,
+                preloadedState: {
+                    login: {
+                        isLoggedIn: true,
+                        isLoading: false,
+                    },
+                },
+            });
+            const heading = await screen.findByRole('heading', {
+                name: elementHeadline,
+                level: level,
+            });
+            expect(heading).toBeInTheDocument();
+        },
+    );
+
+    it('should render ErrorPage if calling a not existing route', async () => {
+        navigateTo({
+            route: '/does-not-exist',
+            preloadedState: {
+                login: {
+                    isLoggedIn: true,
+                    isLoading: false,
+                },
+            },
+        });
+        const heading = await screen.findByRole('heading', {
+            name: /seite nicht gefunden/i,
+            level: 5,
+        });
+        expect(heading).toBeInTheDocument();
+    });
+
+    it('should render the ResetPasswordPage', async () => {
+        render(
+            <Provider store={store}>
+                <MemoryRouter
+                    initialEntries={['/reset-password?token=test123&email=test@example.com']}
+                >
+                    <ResetPasswordPage />
+                </MemoryRouter>
+            </Provider>,
+        );
+
+        const heading = await screen.findByRole('heading', { name: /Passwort zurücksetzen/i });
+        expect(heading).toBeInTheDocument();
+    });
 });
