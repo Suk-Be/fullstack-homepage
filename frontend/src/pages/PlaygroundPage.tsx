35c6cc919a4faada454f352727e271e352000539
Suk-Be Jang <s.jang@sokdesign.de>
2025-07-30 11:53:34 +0200

    delete obsolete components

--- frontend/src/pages/PlaygroundPage.tsx
+++ frontend/src/pages/PlaygroundPage.tsx
@@ -1,11 +1,9 @@
-import { RegisterForm } from '@/components/RegisterForm';
 import { Box } from '@mui/material';
 
 const PlaygroundPage = () => {
     return (
         <Box sx={{ padding: '7rem 0rem' }}>
             <h1>Playground</h1>
-            <RegisterForm />
             {/* <LoginTest />
             <ToggleSignIn /> */}
         </Box>
