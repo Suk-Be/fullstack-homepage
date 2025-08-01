bec598e5134ae7a0dc237c074dc8b8a57b858020
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-01 10:54:02 +0200

    Rename ParenTag to WrappingTag since it is used as Closing and Opening Tag

--- frontend/src/componentsTemplateEngine/gridConfiguration/markUp/generatorElements/WrappingTag.tsx
+++ frontend/src/componentsTemplateEngine/gridConfiguration/markUp/generatorElements/WrappingTag.tsx
@@ -1,27 +1,34 @@
 import { toTextClosingTagFrom, toTextOpeningTagFrom } from '@/utils/templateEngine/parseHtmlToText';
 import { FC, JSXElementConstructor, ReactElement, ReactNode } from 'react';
 
-type ParentTagProps = {
+type WrappingTagProps = {
     isOpeningTag?: boolean;
     isClosingTag?: boolean;
     Component: ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode>;
 };
 
-const ParentTag: FC<ParentTagProps> = ({
+const WrappingTag: FC<WrappingTagProps> = ({
     isOpeningTag = false,
     isClosingTag = false,
     Component,
 }) => {
+    const styleSpacingAroundCodeTop = "block pb-4"
+    const styleSpacingAroundCodeBottom = "block pt-4"
+
     const Tag = () => {
         if (isOpeningTag) {
-            return <code className="block pb-4">{toTextOpeningTagFrom(Component)}</code>;
+            return <code className={styleSpacingAroundCodeTop}>
+              {toTextOpeningTagFrom(Component)}
+            </code>;
         }
         if (isClosingTag) {
-            return <code className="block pt-4">{toTextClosingTagFrom(Component)}</code>;
+            return <code className={styleSpacingAroundCodeBottom}>
+              {toTextClosingTagFrom(Component)}
+            </code>;
         }
     };
 
     return <Tag />;
 };
 
-export default ParentTag;
+export default WrappingTag;
