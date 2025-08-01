978db9a16cd263c988e2744985305ea457dc86ac
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-01 10:51:10 +0200

    refactor naming to be more expressive

--- frontend/src/componentsTemplateEngine/presetRenderExamples/SimpleGrid.tsx
+++ frontend/src/componentsTemplateEngine/presetRenderExamples/SimpleGrid.tsx
@@ -1,17 +1,17 @@
 import GridElement from '@/componentsTemplateEngine/gridConfiguration/markUp/generatorElements/GridElement';
 import { twMerge } from 'tailwind-merge';
 
-const simpleGridArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const gridItemsAmount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-const makeSimpleGrid = (array: Array<number>) => (className: string) =>
+const makeGridItems = (array: Array<number>) => (className: string) =>
     array.map((i) => {
         return <GridElement key={i.toString()} className={className} />;
     });
 
-const SimpleGrid = ({ className }: { className?: string }) => {
+const SimpleGrid = ({ layoutGapConfig }: { layoutGapConfig?: string }) => {
     return (
-        <div className={twMerge('grid grid-cols-3 xl:grid-cols-5 px-4 py-4', className)}>
-            {makeSimpleGrid(simpleGridArray)('h-24 bg-gray-light')}
+        <div className={twMerge('grid grid-cols-3 xl:grid-cols-5 px-4 py-4', layoutGapConfig)}>
+            {makeGridItems(gridItemsAmount)('h-24 bg-gray-light')}
         </div>
     );
 };
