b8053f051383e5146a1b8f56ed42c34e95ce401a
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-03 09:00:34 +0200

    refactor code  style

--- frontend/src/componentsTemplateEngine/presetRenderExamples/SpanningGridPreset.tsx
+++ frontend/src/componentsTemplateEngine/presetRenderExamples/SpanningGridPreset.tsx
@@ -1,33 +1,57 @@
 import MarkupGenerator from '@/componentsTemplateEngine/presetRenderExamples/ctaGenerateMarkup';
 import ColspanningGridMarkup from '@/componentsTemplateEngine/presetRenderExamples/ctaGenerateMarkup/generateMarkup/ColspanningGridMarkup';
 import RowspanningGridMarkup from '@/componentsTemplateEngine/presetRenderExamples/ctaGenerateMarkup/generateMarkup/RowspanningGridMarkup';
+import RowspanningGrid from '@/componentsTemplateEngine/presetRenderExamples/grids/RowspanningGrid';
 import { testId } from '@/utils/testId';
-import ColspanningGrid from './ColspanningGrid';
+import ColspanningGrid from './grids/ColspanningGrid';
 import Headline from './Headline';
-import RowspanningGrid from './RowspanningGrid';
+
+type LayoutColSpanConfigValue = 'col-span-2';
 
 type SpanningGridPresetProps = {
     heading: string;
-    colSpan?: 'col-span-2';
+    layoutColSpanConfig?: LayoutColSpanConfigValue;
     testID: string;
 };
 
-const SpanningGridPreset = ({ heading, colSpan, testID }: SpanningGridPresetProps) => {
+const ColSpanGrid = ({
+    layoutColSpanConfig,
+}: {
+    layoutColSpanConfig: LayoutColSpanConfigValue;
+}) => (
+    <div className="flex flex-col">
+        <div className="w-full h-full">
+            <div className="w-full h-full">
+                <ColspanningGrid layoutConfig={layoutColSpanConfig} />
+            </div>
+        </div>
+
+        <MarkupGenerator component={<ColspanningGridMarkup layoutConfig={layoutColSpanConfig} />} />
+    </div>
+);
+
+const RowspanGrid = () => (
+    <div className="flex flex-col">
+        <div className="w-full h-f  ull">
+            <div className="w-full h-full">
+                <RowspanningGrid />
+            </div>
+        </div>
+
+        <MarkupGenerator component={<RowspanningGridMarkup />} />
+    </div>
+);
+
+const SpanningGridPreset = ({ heading, layoutColSpanConfig, testID }: SpanningGridPresetProps) => {
     return (
         <section className="w-full mx-auto" {...testId(testID)}>
             <Headline>{heading}</Headline>
-            <div className="flex flex-col">
-                <div className="w-full h-full">
-                    <div className="w-full h-full">
-                        {colSpan ? <ColspanningGrid className={colSpan} /> : <RowspanningGrid />}
-                    </div>
-                </div>
-                {colSpan ? (
-                    <MarkupGenerator component={<ColspanningGridMarkup hasCol={colSpan} />} />
-                ) : (
-                    <MarkupGenerator component={<RowspanningGridMarkup />} />
-                )}
-            </div>
+
+            {layoutColSpanConfig ? (
+                <ColSpanGrid layoutColSpanConfig={layoutColSpanConfig} />
+            ) : (
+                <RowspanGrid />
+            )}
         </section>
     );
 };
