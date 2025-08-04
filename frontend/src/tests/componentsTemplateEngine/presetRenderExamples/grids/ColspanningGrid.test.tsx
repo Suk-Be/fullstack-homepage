3cc5e960d0cd23cdd496f8075e7f7e23fc3c11b1
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-04 11:32:13 +0200

    replace tests

--- frontend/src/tests/componentsTemplateEngine/presetRenderExamples/grids/ColspanningGrid.test.tsx
+++ frontend/src/tests/componentsTemplateEngine/presetRenderExamples/grids/ColspanningGrid.test.tsx
@@ -1,17 +1,36 @@
 import ColspanningGrid from '@/componentsTemplateEngine/presetRenderExamples/grids/ColspanningGrid';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
 
 describe('ColspanningGrid', () => {
-    it('applies layoutConfig class to specific GridElements', () => {
+    const renderUtils = () => {
         const layoutConfig = 'col-span-2';
-
         render(<ColspanningGrid layoutConfig={layoutConfig} />);
 
-        const gridContainer = screen.getByTestId('col-spannning-grid-container');
-        expect(gridContainer).toBeInTheDocument();
+        const container = screen.getByTestId('col-spannning-grid-container');
+        return {
+            container,
+            layoutConfig,
+        };
+    };
+    it('renders the grid container with correct classes', () => {
+        const { container } = renderUtils();
 
-        const layoutConfigApplied = gridContainer.querySelectorAll(`.${layoutConfig}`);
-        expect(layoutConfigApplied).toHaveLength(3);
+        expect(container).toBeInTheDocument();
+        expect(container).toHaveClass('grid grid-cols-3 xl:grid-cols-3 gap-4 px-4 py-4');
+    });
+
+    it('renders exactly 6 GridElement children with correct classes', () => {
+        const { container, layoutConfig } = renderUtils();
+        const children = container.children;
+
+        expect(children).toHaveLength(6);
+
+        expect(children[0]).toHaveClass('h-24 bg-green-dark');
+        expect(children[1]).toHaveClass('h-24 bg-green', layoutConfig);
+        expect(children[2]).toHaveClass('h-24 bg-green', layoutConfig);
+        expect(children[3]).toHaveClass('h-24 bg-green-dark');
+        expect(children[4]).toHaveClass('h-24 bg-green-dark');
+        expect(children[5]).toHaveClass('h-24 bg-green', layoutConfig);
     });
 });
--- frontend/src/tests/componentsTemplateEngine/presetRenderExamples/grids/SimpleGrid.test.tsx
+++ frontend/src/tests/componentsTemplateEngine/presetRenderExamples/grids/SimpleGrid.test.tsx
@@ -1,35 +1,31 @@
 import SimpleGrid from '@/componentsTemplateEngine/presetRenderExamples/grids/SimpleGrid';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { describe, expect, it, vi } from 'vitest';
-
-vi.mock(
-    '@/componentsTemplateEngine/gridConfiguration/markUp/generatorElements/GridElement',
-    () => ({
-        default: ({ className }: { className: string }) => (
-            <div data-testid="grid-element" className={className}>
-                GridElement
-            </div>
-        ),
-    }),
-);
 
 describe('SimpleGrid', () => {
-    it('should render 12 GridElement components', () => {
-        render(<SimpleGrid />);
+    const renderUtils = () => {
+        const layoutConfig = 'gap-4';
+        render(<SimpleGrid layoutGapConfig={layoutConfig} />);
+        const container = screen.getByTestId('grid-container');
+        return {
+            container,
+            layoutConfig,
+        };
+    };
+    it('renders the grid container with correct classes', () => {
+        const { container, layoutConfig } = renderUtils();
 
-        const gridElements = screen.getAllByTestId('grid-element');
-
-        expect(gridElements).toHaveLength(12);
+        expect(container).toBeInTheDocument();
+        expect(container).toHaveClass(`grid grid-cols-3 xl:grid-cols-5 px-4 py-4 ${layoutConfig}`);
     });
 
-    it('should apply the layoutGapConfig class to each GridElement', () => {
-        const layoutGapConfig = 'gap-4';
-        render(<SimpleGrid layoutGapConfig={layoutGapConfig} />);
+    it('renders right count of children with correct classes', () => {
+        const { container } = renderUtils();
+        const children = container.children;
 
-        const gridElements = screen.getAllByTestId('grid-element');
-        expect(gridElements[0].className).toContain('h-24 bg-gray-light');
-
-        const gridContainerWithTestId = screen.getByTestId('grid-container');
-        expect(gridContainerWithTestId).toHaveClass(layoutGapConfig);
+        expect(children).toHaveLength(12);
+        Array.from(children).forEach((element) => {
+            expect(element as HTMLElement).toHaveClass('h-24 bg-gray-light');
+        });
     });
 });
