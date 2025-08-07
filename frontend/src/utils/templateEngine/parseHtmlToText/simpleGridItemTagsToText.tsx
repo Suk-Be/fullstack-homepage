a6b98848743165535fa5482343b113c7475a3922
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-07 11:10:31 +0200

    refactor name and add test

--- frontend/src/utils/templateEngine/parseHtmlToText/simpleGridItemTagsToText.tsx
+++ frontend/src/utils/templateEngine/parseHtmlToText/simpleGridItemTagsToText.tsx
@@ -1,11 +1,12 @@
 import SimpleGrid from '@/componentsTemplateEngine/presetRenderExamples/grids/SimpleGrid';
 import {
-    createHtmlAsTextFromPassedComponent,
-    parseStringToADomModel,
+  createHtmlAsTextFromPassedComponent,
+  parseStringToADomModel,
 } from '@/utils/templateEngine/parseHtmlToText/index';
 
 /**
  * Helper function to get a list of texts from the child nodes from a passed in react component
+ * THis one is used on SimpleGrid Markup for Grid Example Page
  * @param Component: react component
  * @returns array of strings
  * example toTextChildNodesFrom(<SimpleGrid className="gap-4" />);
@@ -16,10 +17,12 @@ const domSimpleGrid = parseStringToADomModel(
     createHtmlAsTextFromPassedComponent(<SimpleGrid layoutGapConfig="gap-4" />),
 ).body.firstChild?.childNodes;
 
-const simpleGridChildrenTagsToText = Array.from(domSimpleGrid ?? []).map((elem) => {
-    let tmp = document.createElement('div');
-    tmp.appendChild(elem);
-    return tmp?.innerHTML;
+const simpleGridItemTagsToText: string[] = Array.from(domSimpleGrid ?? []).map((elem) => {
+    let gridItem = document.createElement('div');
+    gridItem.appendChild(elem);
+    // console.log('simpleGridItemTagsToText: ', gridItem.appendChild(elem))
+    // logs: <div class="w-full rounded-xl h-24 bg-gray-light"></div>
+    return gridItem?.innerHTML;
 });
 
-export default simpleGridChildrenTagsToText;
+export default simpleGridItemTagsToText;
