4d5b259799005aba25d42b61c225fc296c7beee6
Suk-Be Jang <s.jang@sokdesign.de>
2025-08-25 19:39:23 +0200

    fix layout

--- frontend/src/componentsTemplateEngine/pageContainers/layoutConfigurator/AsideRight.tsx
+++ frontend/src/componentsTemplateEngine/pageContainers/layoutConfigurator/AsideRight.tsx
@@ -19,7 +19,7 @@ const AsideRight = ({ children }: PropsWithChildren) => {
             <div
                 className="
                   w-full h-full mx-8 lg:mx-0
-                  grid grid-cols-2 lg:grid-cols-1 gap-8"
+                  grid grid-cols-3 lg:grid-cols-1 gap-8"
             >
                 {children}
             </div>
