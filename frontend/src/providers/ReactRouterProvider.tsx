8946065040e77c9bf2dd2db913023fa406f6168a
Suk-Be Jang <s.jang@sokdesign.de>
2025-11-17 07:12:40 +0100

    add dynamic base route

--- frontend/src/providers/ReactRouterProvider.tsx
+++ frontend/src/providers/ReactRouterProvider.tsx
@@ -1,7 +1,9 @@
 import routes from '@/routes';
 import { RouterProvider, createBrowserRouter } from 'react-router';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+    basename: import.meta.env.VITE_BASENAME,
+});
 
 const ReactRouterProvider = () => <RouterProvider router={router} />;
 
