c13a06d3b67bd33151ddc761a4eed0d616c0c17f
Suk-Be Jang <s.jang@sokdesign.de>
2025-12-12 16:45:11 +0100

    Style E-Mail Notifiaction to be in German

--- backend/app/Notifications/ResetPasswordNotification.php
+++ backend/app/Notifications/ResetPasswordNotification.php
@@ -34,15 +34,27 @@ class ResetPasswordNotification extends Notification implements ShouldQueue
             return call_user_func(static::$toMailCallback, $notifiable, $this->token);
         }
 
-        $frontendUrl = env('FRONTEND_URL', 'http://localhost:5173') . '/reset-password?token=' . $this->token . '&email=' . $notifiable->getEmailForPasswordReset();
+        $frontendUrl = env('FRONTEND_URL', 'http://localhost:5173')
+            . '/reset-password?token=' . $this->token
+            . '&email=' . $notifiable->getEmailForPasswordReset();
 
 
         return (new MailMessage)
-            ->subject(Lang::get('Passwort zurücksetzen'))
-            ->line(Lang::get('Sie erhalten diese E-Mail, weil wir eine Anfrage zum Zurücksetzen des Passworts für Ihr Konto erhalten haben.'))
-            ->action(Lang::get('Passwort zurücksetzen'), $frontendUrl) // Verwenden Sie Ihre Frontend-URL hier!
-            ->line(Lang::get('Dieser Passwort-Reset-Link läuft in :count Minuten ab.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
-            ->line(Lang::get('Wenn Sie kein Zurücksetzen des Passworts angefordert haben, ist keine weitere Aktion erforderlich.'));
+            ->subject('Passwort zurücksetzen')
+            ->greeting('Lieber User,')
+            ->line('Sie erhalten diese E-Mail, weil wir eine Anfrage zum Zurücksetzen des Passworts für Ihr Konto erhalten haben.')
+            ->action('Passwort zurücksetzen', $frontendUrl)
+            ->line('Dieser Passwort-Reset-Link läuft in :count Minuten ab.', [
+                'count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire'),
+            ])
+            ->line('Wenn Sie kein Zurücksetzen des Passworts angefordert haben, ist keine weitere Aktion erforderlich.')
+            ->line('')
+            ->line('Viele Grüße')
+            ->line('Suk-Be Jang')
+            ->salutation(' ');
+
+
+
     }
 
     public static function createUrlUsing($callback)
