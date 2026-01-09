77516824feec3b73fe1fee7dc14b169366965024
Suk-Be Jang <s.jang@sokdesign.de>
2026-01-09 12:52:03 +0100

    add test for UnitOfMeasurement, fix typo elements to element

--- frontend/src/componentsTemplateEngine/gridConfiguration/ui/shared-comnponents/UnitOfMeasurement.tsx
+++ frontend/src/componentsTemplateEngine/gridConfiguration/ui/shared-comnponents/UnitOfMeasurement.tsx
@@ -20,7 +20,6 @@ const UnitOfMeasurement = ({ unit, numerator, denominator, suffix = 'rem' }: Pro
     }
 
     if (unit === 'px') return <p>Unit: px</p>;
-
     if (unit === 'element') return <p>Unit: element</p>;
 
     return null;
