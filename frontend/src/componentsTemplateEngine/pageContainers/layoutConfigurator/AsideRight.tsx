0f143386f688fad7cb838c248ee1d4b88dac53fd
Suk-Be Jang <s.jang@sokdesign.de>
2026-01-14 16:53:36 +0100

    simplify margin

--- frontend/src/componentsTemplateEngine/pageContainers/layoutConfigurator/AsideRight.tsx
+++ frontend/src/componentsTemplateEngine/pageContainers/layoutConfigurator/AsideRight.tsx
@@ -5,15 +5,15 @@ const AsideRight = ({ children }: PropsWithChildren) => {
     return (
         <aside
             className="
-            flex order-3 flex-wrap 
-            rounded-lg 
-            bg-gray-dark text-white 
-            m-auto lg:m-0 
-            w-full lg:w-1/6 
-            md:p-8 lg:p-4 
-            border-2 
-            overflow-y-auto 
-            max-h-full max-height: 95.3vh"
+            flex order-3 flex-wrap
+            rounded-lg
+            bg-gray-dark text-white
+            m-0
+            w-full lg:w-1/6
+            md:p-8 lg:p-4
+            border-2
+            overflow-y-auto
+            max-h-full max-height:95.3vh"
             {...testId('aside-right')}
         >
             <div
