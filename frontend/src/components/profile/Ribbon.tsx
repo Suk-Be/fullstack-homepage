4e7177cc0545cbff4b150d452bf96ff0721349a3
Suk-Be Jang <s.jang@sokdesign.de>
2026-01-27 12:57:08 +0100

    enable dark mode

--- frontend/src/components/profile/Ribbon.tsx
+++ frontend/src/components/profile/Ribbon.tsx
@@ -15,24 +15,37 @@ interface HeadlineProps extends PropsWithChildren {
 const RibbonLayout = ({ children, variant = 'h2', component = 'h2' }: HeadlineProps) => {
     return (
         <Box
-            sx={{
+            sx={(theme) => ({
                 color: 'rgb(33,29,29)',
                 fontFamily: 'Fira Sans',
                 fontWeight: 400,
                 fontSize: '1rem',
                 fontStyle: 'normal',
+
+                // Light Mode (grün)
                 background: 'rgb(56,255,148)',
+
                 position: 'absolute',
                 inset: '0 calc(-1*2rem) auto auto',
                 padding: '20px 20px 30px 2rem',
-                clipPath: `polygon(0 0, 100% 0, 100% calc(100% - 10px), 
-                                  calc(100% - 2rem) 100%, 
-                                  calc(100% - 2rem) calc(100% - 10px), 
-                                  0 calc(100% - 10px), 
-                                  0px calc(50% - 10px/2))`,
+                clipPath: `polygon(
+                  0 0, 
+                  100% 0, 
+                  100% calc(100% - 10px), 
+                  calc(100% - 2rem) 100%, 
+                  calc(100% - 2rem) calc(100% - 10px), 
+                  0 calc(100% - 10px), 
+                  0px calc(50% - 10px/2)
+                )`,
                 boxShadow: '0 calc(-1*10px) 0 inset #0005',
                 width: 'calc(100% + 2rem)',
-            }}
+
+                // Dark Mode (hellgrau)
+                ...theme.applyStyles('dark', {
+                    background: 'hsl(0, 0%, 90%)',
+                    color: 'rgba(0,0,0, 1)',
+                }),
+            })}
         >
             <Typography
                 variant={variant}
@@ -49,4 +62,5 @@ const RibbonLayout = ({ children, variant = 'h2', component = 'h2' }: HeadlinePr
         </Box>
     );
 };
+
 export default RibbonLayout;
