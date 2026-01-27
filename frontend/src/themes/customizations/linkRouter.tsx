e8a0d4eaf050743f0ae1bfe07346f1ebb20dd2bd
Suk-Be Jang <s.jang@sokdesign.de>
2026-01-27 12:54:44 +0100

    refactor import

--- frontend/src/themes/customizations/linkRouter.tsx
+++ frontend/src/themes/customizations/linkRouter.tsx
@@ -1,6 +1,6 @@
+import RouterLinkWrapper from '@/components/RouterLink';
 import { LinkProps } from '@mui/material/Link';
 import { Components, Theme } from '@mui/material/styles';
-import RouterLinkWrapper from '../../components/RouterLink';
 
 const routerLinkCustomizations: Components<Theme> = {
     MuiLink: {
