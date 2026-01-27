0dc6e5c8c55a6a9ff886b85c59320ee4ec998d4a
Suk-Be Jang <s.jang@sokdesign.de>
2026-01-27 17:09:07 +0100

    cleanup imports

--- frontend/src/providers/index.tsx
+++ frontend/src/providers/index.tsx
@@ -1,12 +1,15 @@
+import AuthInitializer from '@/providers/AuthInitializer';
+import MUIThemeProvider from '@/providers/MUIThemeProvider';
+import ReactRouterProvider from '@/providers/ReactRouterProvider';
+import ReduxProvider from '@/providers/ReduxProvider';
+import { useColorSchemeAttribute } from '@/themes/useColorSchemeAttribute';
 import { loadRecaptchaScript } from '@/utils/recaptcha/recaptchaToken';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useEffect } from 'react';
-import AuthInitializer from './AuthInitializer';
-import MUIThemeProvider from './MUIThemeProvider';
-import ReactRouterProvider from './ReactRouterProvider';
-import ReduxProvider from './ReduxProvider';
 
 const Providers = () => {
+    useColorSchemeAttribute();
+
     useEffect(() => {
         loadRecaptchaScript(); // Nur Skript preloaden – kein Token!
     }, []);
